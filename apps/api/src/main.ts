eabe8d55e62601bc308f0c70f501758f02de60db
rameessiyad <rameesmsiyadofficial786@gmail.com>
2026-08-08 08:18:10 +0530

    feat: initialized next admin web frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
